15d49a22a8c7dfbb181c105e815e749ec4513f1e
Nathan Sala <natouda@gmail.com>
2020-04-04 02:03:12 +0300

    login accept POST request.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app
 from app.forms import LoginForm
-from flask import render_template
+from flask import render_template, flash, redirect
 
 
 @app.route('/')
@@ -26,7 +26,11 @@ def index():
     return render_template('index.html', title='Home', user=user, projects=projects)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login requested for use: {form.username.data} remember_me: {form.remember_me.data}')
+        return redirect('/index')
+
     return render_template('login.html', title='Sign In', form=form)
