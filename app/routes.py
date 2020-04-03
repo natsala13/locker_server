d01f7394b4a831a9373c27b1bbcb44cacea2d4fc
Nathan Sala <natouda@gmail.com>
2020-04-03 13:19:54 +0300

    Added Templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,25 @@
 from app import app
+from flask import render_template
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Nathan'}
+
+    projects = [
+        {
+            'id': 1,
+            'tutor': {'username': 'Yaron'},
+            'team': [{'username': 'Shteren'}, {'username': 'Nathan'}],
+            'description': 'Locker manager website'
+        },
+        {
+            'id': 2,
+            'tutor': {'username': 'Boaz'},
+            'team': [{'username': 'Nati'}, {'username': 'Shteren'},  {'username': 'Nathan'}],
+            'description': 'Autonumus Arduino project'
+        }
+    ]
+
+    return render_template('index.html', title='Home', user=user, projects=projects)
