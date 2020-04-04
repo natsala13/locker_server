998ef47184025b6f1354b3f08308249709e71928
Nathan Sala <natouda@gmail.com>
2020-04-04 16:57:21 +0300

    Added db configurations

--- config.py
+++ config.py
@@ -1,5 +1,12 @@
 import os
 
 
+DIRECTORY_PATH = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
+    sql_default_url = 'sqlite:///' + os.path.join(DIRECTORY_PATH, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or sql_default_url
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
