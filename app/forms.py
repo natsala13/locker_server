0f34804bdcfe9d936b5a0eb9513abb8fa6057a6c
Nathan Sala <natouda@gmail.com>
2020-04-12 02:24:59 +0300

    registration form

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,8 @@
+from app.models import User
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
 
 
 class LoginForm(FlaskForm):
@@ -8,3 +10,21 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+
+class RegistrarionForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = StringField('Password', validators=[DataRequired()])
+    password_repeat = StringField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Submit')
+
+    def validate_username(self, username):
+        u = User.query.filter(User.username == username.data).first()
+        if u:
+            raise ValidationError('username already existing, please choose another one')
+
+    def validate_email(self, email):
+        u = User.query.filter(User.email == email.data).first()
+        if u:
+            raise ValidationError('email already in use, please use another one')
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import app
+from app import db
 from app.models import User
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrarionForm
 
 from werkzeug.urls import url_parse
 from flask import render_template, flash, redirect, url_for, request
@@ -63,5 +64,22 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
-    from getmac import get_mac_address as gma
 
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    form = RegistrarionForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Congratulation {user.username}! You were registered successfully!')
+
+        return redirect(url_for('login'))
+
+    return render_template('register.html', title='Register', form=form)
