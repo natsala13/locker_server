9bd210bd1c6e5e6777b7760394f52deaf21262db
Nathan Sala <natouda@gmail.com>
2020-04-15 12:28:28 +0300

    Item add title

--- app/models.py
+++ app/models.py
@@ -49,6 +49,7 @@ def load_user(user_id):
 
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(40))
     description = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
